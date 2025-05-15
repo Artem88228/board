a58fa13a2d093cd128d05f9b20f0961c073dfc94
Artem <artembargatin@gmail.com>
2025-05-15 17:48:20 +0600

    Task-2

--- task-2/board.ts
+++ task-2/board.ts
@@ -1,1 +1,9 @@
-console.log('sasf');
+const size = 8;
+
+for (let row = 0; row < size; row++) {
+  let line = "";
+  for (let col = 0; col < size; col++) {
+    line += (row + col) % 2 === 0 ? "⬛" : "⬜";
+  }
+  console.log(line);
+}
