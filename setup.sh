3e1d3abee7b896c194929d514ce78a55e7ccfe92
Artem <artembargatin@gmail.com>
2025-05-15 15:01:38 +0600

    Task 1

---
+++ setup.sh
@@ -0,0 +1,5 @@
+mkdir -p projects/my_project/directory1/subdirectory1
+mkdir -p projects/my_project/directory2
+mkdir -p projects/my_project/directory3/subdirectory2
+
+echo 'Hello from JS' > projects/my_project/directory2/hello.txt
